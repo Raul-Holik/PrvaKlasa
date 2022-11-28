14fad062be167e0b3cfbcd81d0f8333de8782983
Raul-Holik <113007938+Raul-Holik@users.noreply.github.com>
2022-11-28 18:05:33 +0100

    Add files via upload

---
+++ PrvaKlasa/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrvaKlasa
+{
+    class PrvaKlasa
+    {
+        private static int counter;
+
+        public PrvaKlasa()
+        {
+            counter +=1;
+            return;
+        }
+        public int Counter()
+        {
+            return counter;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrvaKlasa x = new PrvaKlasa();
+            x.Counter();            
+            Console.WriteLine(x.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
